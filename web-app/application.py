e5014ec1eb9df2c0b7c0f025a91942f30db2e213
Jeff Katz <JeffreyEricKatz@gmail.com>
2020-12-03 17:33:00 -0500

    update all

---
+++ web-app/application.py
@@ -0,0 +1,28 @@
+import sqlite3
+from flask import Flask, jsonify
+import os
+import pdb
+
+app = Flask(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'nhl.db'),
+))
+
+@app.route('/nhl/players')
+def players():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    cursor = conn.cursor()
+    cursor.execute('select * from players;')
+    players = cursor.fetchall()
+    return jsonify(players)
+
+@app.route('/nhl/players/<player_id>')
+def show_player(player_id):
+    conn = sqlite3.connect(app.config['DATABASE'])
+    cursor = conn.cursor()
+   cursor.execute('select * from players where id = ?;', (player_id,))
+   player_details = cursor.fetchone()
+   jsonify(player_details)
+
+app.run(debug = True)
---
+++ web-app/player.py
@@ -0,0 +1,11 @@
+class Player:
+    def __init__(self, id, name, age, height, weight, shot, birth_place, birthdate, number):
+        self.id = id
+        self.name = name
+        self.age = age
+        self.height = height
+        self.weight = weight
+        self.shot = shot
+        self.birth_place = birth_place
+        self.birthdate = birthdate
+        self.number = number
